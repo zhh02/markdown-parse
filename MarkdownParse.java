7521f44ef7aaa8281323167f22557f097edb985e
zhh02 <86755042+zhh02@users.noreply.github.com>
2022-02-24 17:07:32 -0800

    Update to group version cr atroung39

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,39 +3,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        int currentIndex = 0;
-        int lastClosedParen=markdown.lastIndexOf(")");
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextOpenCarrot = markdown.indexOf("<", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int nextCloseCarrot = markdown.indexOf(">", nextOpenCarrot);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            System.out.println(currentIndex);
-            currentIndex = closeParen + 1;
-            if (nextOpenBracket == -1 || nextCloseBracket == -1 ||
-            nextOpenCarrot == -1 || nextCloseCarrot == -1 ||
-            openParen == -1 || closeParen == -1||closeParen==lastClosedParen){
-                break;
-            }
-            if(markdown.charAt(nextOpenBracket-1) != '!'){
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            }
-            toReturn.add(markdown.substring(nextOpenCarrot + 1, nextCloseCarrot));
-            
+        String regex = "(?<!!)\\[.+\\]\\(\\h*(\\S+)\\h*\\)";
+        Matcher matcher = Pattern.compile(regex).matcher(markdown);
+
+        while(matcher.find()) {
+            toReturn.add(matcher.group(1));
         }
-        
-        System.out.println(currentIndex);
-        int test = markdown.indexOf("]");
-        System.out.println(test);
+
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
