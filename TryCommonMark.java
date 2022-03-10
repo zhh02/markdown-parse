b865613bafa37e6ea2b3c4f8020d680ccd3690f3
zhh02 <86755042+zhh02@users.noreply.github.com>
2022-03-10 09:59:28 -0800

    Update TryCommonMark.java

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -1,12 +1,18 @@
-// this part actually does the computation
-Node node = parser.parse("Example\n=======\n\nSome more text");
-WordCountVisitor visitor = new WordCountVisitor();
-node.accept(visitor);
-visitor.wordCount;  // 4
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 
 // this class can be defined anywhere in the file
 class WordCountVisitor extends AbstractVisitor {
     int wordCount = 0;
+    
+    public static void main(String[] args) {
+        // this part actually does the computation
+        Node node = parser.parse("Example\n=======\n\nSome more text");
+        WordCountVisitor visitor = new WordCountVisitor();
+        node.accept(visitor);
+        visitor.wordCount;  // 4
+    }
 
     @Override
     public void visit(Text text) {
@@ -17,5 +23,14 @@ class WordCountVisitor extends AbstractVisitor {
 
         // Descend into children (could be omitted in this case because Text nodes don't have children).
         visitChildren(text);
+    }
+}
+
+class TryCommonMark {
+    public static void main(String[] args) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse("This is *Sparta*");
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
     }
 }
