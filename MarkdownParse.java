2d2cc40a26d2a98718d315be9b114ad72889c461
zhh02 <86755042+zhh02@users.noreply.github.com>
2022-03-11 11:57:29 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,4 +1,4 @@
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+/* File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -86,6 +86,36 @@ public class MarkdownParse {
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String contents = Files.readString(fileName);
+        ArrayList<String> links = getLinks(contents);
+        System.out.println(links);
+    }
+}
+*/
+
+// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+// MarkdownParse of the group from lab 7 credit to @atruong39
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MarkdownParse {
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        String regex = "(?<!!)\\[.+\\]\\(\\h*(\\S+)\\h*\\)";
+        Matcher matcher = Pattern.compile(regex).matcher(markdown);
+
+        while(matcher.find()) {
+            toReturn.add(matcher.group(1));
+        }
+
+        return toReturn;
+    }
+    public static void main(String[] args) throws IOException {
+		Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
